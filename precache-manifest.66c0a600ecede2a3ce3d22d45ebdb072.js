7485cc5905329b4157a743c311d775367ae34bda
Ricardo Brambila <ricardo.brambila.jsx@gmail.com>
2019-07-12 11:56:37 -0300

    Updates

---
+++ precache-manifest.66c0a600ecede2a3ce3d22d45ebdb072.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bc50fa586f98ba26161a76fcb9b8dcd",
+    "url": "/ricardo-jsx/desafio-starwars.git/index.html"
+  },
+  {
+    "revision": "ee3fd880684fca960812",
+    "url": "/ricardo-jsx/desafio-starwars.git/static/js/2.ad0fe263.chunk.js"
+  },
+  {
+    "revision": "9c902a0f7418b514a0de",
+    "url": "/ricardo-jsx/desafio-starwars.git/static/js/main.f17d23e6.chunk.js"
+  },
+  {
+    "revision": "8e265fbf2209e306a72f",
+    "url": "/ricardo-jsx/desafio-starwars.git/static/js/runtime~main.e9e2a1ec.js"
+  }
+]);
